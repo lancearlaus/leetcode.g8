f2c265a9c3e72b98ac91f6610444511ba06fe5e0
Lance Arlaus <lance.arlaus@gmail.com>
2020-01-16 09:13:13 -0500

    Initial template

---
+++ src/main/g8/src/main/scala/$problem__Camel$/Solution.scala
@@ -0,0 +1,12 @@
+package $problem;format=Camel$
+
+/**
+    LeetCode problem solution
+    Name: $name$
+    URL: https://leetcode.com/problems/$problem;format=normalize$
+*/
+object Solution {
+
+    // Replace this line with LeetCode starting code
+
+}
---
+++ src/main/g8/src/test/scala/$problem__Camel$/SolutionTest.scala
@@ -0,0 +1,15 @@
+package $problem;format=Camel$
+
+import org.scalatest.{Matchers, WordSpec}
+
+class SolutionTest extends WordSpec with Matchers {
+
+    "Solution" should {
+
+        "solve" in {
+            
+        }
+
+    }
+
+}
